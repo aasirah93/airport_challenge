b3535df89f2294c199eeba4185275322ee843877
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-07-11 12:06:36 +0100

    Airport responds to take_off with 1 argument

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,7 +1,13 @@
 require 'airport'
+  
 
 describe Airport do
-    it 'responds to land with 1 argument' do
-        expect(subject).to respond_to(:land).with(1).argument
-    end
+  subject(:airport) { described_class.new }
+
+  it 'responds to land with 1 argument' do
+    expect(subject).to respond_to(:land).with(1).argument
+  end
+  it 'responds to take_off with 1 arguement' do
+    expect(subject).to respond_to(:take_off).with(1).argument
+end
 end
