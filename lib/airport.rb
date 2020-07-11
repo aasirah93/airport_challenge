950d4e069c13caaf66334f4b41ae195be311cfda
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-07-11 12:34:06 +0100

    Introduced instance variables

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,9 +1,15 @@
+require_relative 'plane'
+
 class Airport 
+
+  attr_reader :plane
   
   def land(plane)
+    @plane = plane
   end
 
   def take_off(plane)
+    @plane = plane
     return 'it has left the airport'
   end
 
